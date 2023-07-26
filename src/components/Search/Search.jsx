27c15026418aa496bb213af34ea6826b9fd162eb
Polina Ryabova <polinaryabova.2@gmail.com>
2023-07-26 22:33:18 +0500

    add search component

---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import cl from './Search.module.scss';
+
+function Search() {
+  return (
+    <div className={cl.search}>
+      <svg
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="1.25"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <circle cx="11" cy="11" r="8" />
+        <path d="m21 21-4.3-4.3" />
+      </svg>
+      <input className={cl.input__search} placeholder="Search for a country…"></input>
+      <svg
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="1.25"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <circle cx="12" cy="12" r="10" />
+        <path d="m15 9-6 6" />
+        <path d="m9 9 6 6" />
+      </svg>
+    </div>
+  );
+}
+
+export default Search;
