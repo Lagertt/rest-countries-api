7e21c746019e72b7ed6c3d4c23451a47e67b7bb6
Polina Ryabova <polinaryabova.2@gmail.com>
2023-07-31 22:16:50 +0500

    create routes

--- src/App.js
+++ src/App.js
@@ -1,71 +1,16 @@
-import React, { useEffect, useState } from 'react';
-import CountryCard from './components/CountryCard/CountryCard.jsx';
-import Header from './components/Header/Header.jsx';
-import Search from './components/Search/Search.jsx';
-import CountriesService from './API/CountriesService.js';
-import FilterPopup from './components/FilterPopup/FilterPopup.jsx';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import Detail from './pages/Detail.jsx';
 
 function App() {
-  const [countries, setCountries] = useState([]);
-  const [selectedRegion, setSelectedRegion] = useState('');
-  const [searchValue, setSearchValue] = useState('');
-
-  async function fetchCountries() {
-    const countries = await CountriesService.getAll();
-    setCountries(countries);
-  }
-
-  async function filterByRegion(region) {
-    setSelectedRegion(region);
-    const countries = await CountriesService.getByRegion(region);
-    countries && setCountries(countries);
-  }
-
-  async function filterByName(name) {
-    console.log(name);
-    if (name !== '') {
-      const countries = await CountriesService.getByName(name);
-      setCountries(countries);
-    } else {
-      fetchCountries();
-    }
-  }
-
-  useEffect(() => {
-    fetchCountries();
-  }, []);
-
   return (
-    <>
-      <Header title="Where in the world?" />
-      <main className="main">
-        <div className="container">
-          <div className="settings">
-            <Search
-              searchValue={searchValue}
-              setSearchValue={setSearchValue}
-              onSubmit={() => filterByName(searchValue)}
-            />
-            <FilterPopup
-              items={['Africa', 'Americas', 'Asia', 'Europe', 'Oceania']}
-              defaultItem="Filter by Region"
-              value={selectedRegion}
-              onChange={filterByRegion}
-            />
-          </div>
-
-          <div className="countries">
-            {countries ? (
-              countries.map((country, index) => (
-                <CountryCard country={country} index={index} key={`${index}_${country.name}`} />
-              ))
-            ) : (
-              <h1 className="error">По выбранному параметру стран не найдено</h1>
-            )}
-          </div>
-        </div>
-      </main>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:countryName" element={<Detail />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from 'react';
+import Search from '../components/Search/Search.jsx';
+import CountriesService from '../API/CountriesService.js';
+import FilterPopup from '../components/FilterPopup/FilterPopup.jsx';
+import CountryCard from '../components/CountryCard/CountryCard.jsx';
+import Header from '../components/Header/Header.jsx';
+import { Link } from 'react-router-dom';
+
+function Home() {
+  const [countries, setCountries] = useState([]);
+  const [selectedRegion, setSelectedRegion] = useState('');
+  const [searchValue, setSearchValue] = useState('');
+
+  async function fetchCountries() {
+    const countries = await CountriesService.getAll();
+    setCountries(countries);
+  }
+
+  async function filterByRegion(region) {
+    setSelectedRegion(region);
+    const countries = await CountriesService.getByRegion(region);
+    countries && setCountries(countries);
+  }
+
+  async function filterByName(name) {
+    if (name !== '') {
+      const countries = await CountriesService.getByName(name);
+      setCountries(countries);
+    } else {
+      fetchCountries();
+    }
+  }
+
+  useEffect(() => {
+    fetchCountries();
+  }, []);
+
+  return (
+    <>
+      <Header title="Where in the world?" />
+      <main className="main">
+        <div className="container">
+          <div className="settings">
+            <Search
+              searchValue={searchValue}
+              setSearchValue={setSearchValue}
+              onSubmit={() => filterByName(searchValue)}
+            />
+            <FilterPopup
+              items={['Africa', 'Americas', 'Asia', 'Europe', 'Oceania']}
+              defaultItem="Filter by Region"
+              value={selectedRegion}
+              onChange={filterByRegion}
+            />
+          </div>
+
+          <div className="countries">
+            {countries ? (
+              countries.map((country, index) => (
+                <Link to={country.cca2} key={`${index}_${country.name.common}`}>
+                  <CountryCard country={country} index={index} />
+                </Link>
+              ))
+            ) : (
+              <h1 className="error">По выбранному параметру стран не найдено</h1>
+            )}
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default Home;
